7d5f30e9235c90fb8287eacaf05ed44b75cd20c4
klchai <chaikelun@gmail.com>
2020-05-27 06:11:19 +0800

    Update prediction Prophet Model

--- server/README.md
+++ server/README.md
@@ -3,7 +3,7 @@
 # Pré-requis
 
 Executer la commande suivante :
-__apt-get update && apt-get install -y libsasl2-modules libsasl2-dev && pip install --upgrade pip six==1.12.0 "pyhive[hive]" flask__
+__apt-get update && apt-get install -y libsasl2-modules libsasl2-dev && pip install --upgrade pip six==1.12.0 "pyhive[hive]" flask fbprophet statsmodels__
 
 1. __docker-compose up -d__ pour lancer le serveur hive
 2. Attendre une dizaine de secondes pour que le serveur se lance
--- server/prepare.py
+++ server/prepare.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from fbprophet import Prophet
+from statsmodels.tsa.arima_model import ARIMA
 
 def prepare_data():
     print("Begin prepare.")
@@ -9,3 +11,21 @@ def prepare_data():
     headers = ['Country','Province','Confirmed','Recovered','Deaths','Date']
     df.to_csv("res.csv",sep=";",index=False,header=headers)
     print("Prepare finished.")
+
+def predict_Prophet():
+    pred = pd.read_csv("covid-19-all.csv",sep=",")
+    pred = pred.fillna(0)
+    predgrp = pred[pred['Country/Region']=='France'].groupby('Date')['Confirmed','Recovered','Deaths'].sum().reset_index()
+    pred_cnfrm = predgrp.loc[:,["Date","Confirmed"]]
+    pr_data = pred_cnfrm
+    pr_data.columns = ['ds','y']
+    m = Prophet()
+    m.fit(pr_data)
+    future=m.make_future_dataframe(periods=15)
+    forecast=m.predict(future)
+    cnfrm = forecast.loc[:,['ds','trend']]
+    cnfrm = cnfrm[cnfrm['trend']>0]
+    cnfrm.columns = ['Date','Confirm']
+    cnfrm[["Confirm"]] = cnfrm[["Confirm"]].astype(int)
+    res = cnfrm.astype(str).to_json(orient='records',date_format="iso")
+    return res
