5a47fa178a4557421db442f6edd28588c83d4eea
klchai <chaikelun@gmail.com>
2020-05-29 10:47:31 +0200

    Add model arima

--- server/prepare.py
+++ server/prepare.py
@@ -12,10 +12,14 @@ def prepare_data():
     df.to_csv("res.csv",sep=";",index=False,header=headers)
     print("Prepare finished.")
 
-def predict_Prophet():
+def prediction():
     pred = pd.read_csv("covid-19-all.csv",sep=",")
     pred = pred.fillna(0)
     predgrp = pred[pred['Country/Region']=='France'].groupby('Date')['Confirmed','Recovered','Deaths'].sum().reset_index()
+    return predgrp
+
+def predict_Prophet():
+    predgrp = prediction()
     pred_cnfrm = predgrp.loc[:,["Date","Confirmed"]]
     pr_data = pred_cnfrm
     pr_data.columns = ['ds','y']
@@ -27,5 +31,27 @@ def predict_Prophet():
     cnfrm = cnfrm[cnfrm['trend']>0]
     cnfrm.columns = ['Date','Confirm']
     cnfrm[["Confirm"]] = cnfrm[["Confirm"]].astype(int)
-    res = cnfrm.astype(str).to_json(orient='records',date_format="iso")
+    res = cnfrm.astype(str).to_json(orient='records')
     return res
+
+def predict_Arima():
+    predgrp = prediction()
+    pred_cnfrm = predgrp.loc[:,["Date","Confirmed"]]
+    confirm_cs = pred_cnfrm.cumsum()
+    confirm_cs['date1'] = pred_cnfrm['Date']
+    confirm_cs = confirm_cs.drop('Date',axis=1)
+    arima_data = confirm_cs
+    arima_data.columns = ['count','confirmed_date']
+    model = ARIMA(arima_data['count'].values, order=(1, 2, 1))
+    fit_model = model.fit(trend='c', full_output=True, disp=True)
+    forcast = fit_model.forecast(steps=30)
+    pred_y = forcast[0].tolist()
+    pred = pd.DataFrame(pred_y)
+    pred['pred']=  pred - pred.shift(1)
+    predict = pd.DataFrame({'Date':pd.date_range("20200526",periods=29).astype(str),'Confirmed':pred.drop(0)['pred'].astype(int)})
+    pred_cnfrm["Confirmed"] = pred_cnfrm["Confirmed"].astype(int)
+    res = pd.concat([pred_cnfrm, predict])
+    res.reset_index(drop=True,inplace=True)
+    res = res.to_json(orient='records')
+    return res
+
